e25452323a7f577e7e18c98ac86ef2da07f217a4
Eddy Lu <eddy.lu@mail.mcgill.ca>
2015-12-27 23:03:54 -0500

    Initialize basic music player template

---
+++ MusicVisualizer.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta charset="utf-8" />
+        <title>Music Visualizer</title>
+    </head>
+    
+    <body>
+
+        <div id="visualizer"></div>
+
+        <audio id="controls" controls>
+            <source src="song1.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+        </audio>
+
+        <style>
+            #visualizer {
+                background-color: #1A1A1A;
+                height: 337px;
+                width: 600px;
+            }
+            
+            #controls {
+                width: 600px;
+            }            
+        </style>
+
+    </body>
+
+</html>
