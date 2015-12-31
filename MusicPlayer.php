71aa6289e989bdcb7f27d2aac52f8956abc3bd6e
Eddy Lu <eddy.lu@mail.mcgill.ca>
2015-12-30 21:34:05 -0500

    Implement real-time visualizer logo resizing based on music's bass

--- MusicPlayer.php
+++ MusicPlayer.php
@@ -8,14 +8,16 @@
     
     <body>
 
-        <div id="visualizer">
-            <div id="logo"></div>
-        </div>
+        <div id="player">
+            <div id="visualizer">
+                <div id="logo"></div>
+            </div>
 
-        <audio id="controls" controls>
-            <source src="song1.mp3" type="audio/mpeg">
-        Your browser does not support the audio element.
-        </audio>
+            <audio id="controls" controls>
+                <source src="song1.mp3" type="audio/mpeg">
+            Your browser does not support the audio element.
+            </audio>
+        </div>
 
         <p id="log">Hello!</p>
 
@@ -26,7 +28,16 @@
                 background-color: black;
             }
             
+            #player {
+                margin: 0 auto;
+                
+                height: 520px;
+                width: 820px;
+            }
+            
             #visualizer {
+                position: relative;
+                
                 background-image: url(background1.jpg);
                 background-size: 100% auto;
                 height: 500px;
@@ -34,10 +45,13 @@
             }
             
             #visualizer #logo {
-                left: 300px;
-                top: 150px;                
+                position: absolute;
+                
+                top: 50%; left: 50%;
+                transform: translate(-50%, -50%);                         
                 
                 background-image: url(logo.png);
+                background-size: 100% auto;
                 position: absolute;
                 width: 200px;
                 height: 200px;
@@ -74,16 +88,26 @@
                 var bufferLength = analyser.frequencyBinCount;
                 var audioData = new Uint8Array(bufferLength);
 
-                var bassThreshold = 150;
+                var BASS_THRESHOLD = 150;
                 var bassDetected = "";
+
+                var logo = document.getElementById('logo');
+                var LOGO_DEFAULT_WIDTH = 200;
+                var LOGO_DEFAULT_HEIGHT = 200;
 
                 function acquireData() {
                     requestAnimationFrame(acquireData);
                     analyser.getByteFrequencyData(audioData);
                     document.getElementById('log').innerHTML = audioData;
 
-                    if (audioData[0] > bassThreshold) {
+                    if (audioData[0] > BASS_THRESHOLD) {
                         bassDetected += "|";
+                        logo.style.width = (LOGO_DEFAULT_WIDTH + (audioData[0] - BASS_THRESHOLD)) + "px";
+                        logo.style.height = (LOGO_DEFAULT_HEIGHT + (audioData[0] - BASS_THRESHOLD)) + "px";
+                    }
+                    else {
+                        logo.style.width = LOGO_DEFAULT_WIDTH + "px";
+                        logo.style.height = LOGO_DEFAULT_HEIGHT + "px";
                     }
                     document.getElementById('bassDetection').innerHTML = bassDetected;
                 }
