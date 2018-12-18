f84fc491d0acbb77fd606a050a184021d86adaa7
Eddy Lu <eddy.lu@mail.mcgill.ca>
2018-12-18 00:50:55 -0800

    Refactor animation logic
    Replace "var" declarations with "const" and "let"
    Re-order logic
    Separate functions into smaller functions

--- animation.js
+++ animation.js
@@ -1,106 +1,98 @@
 window.onload = function () {
+    const BASS_THRESHOLD = 150;
+    const LOGO_DEFAULT_WIDTH = 200;
+    const LOGO_DEFAULT_HEIGHT = 200;
+    const NUMBER_OF_SNOWFLAKES = 100;
+    const CONFIGURE_SETTING = 10;
 
-    var audio = document.getElementById('controls');
+    const audio = document.getElementById('controls');
+    const logo = document.getElementById('logo');
 
-    var context = new AudioContext();
-    var source = context.createMediaElementSource(audio);
-    var analyser = context.createAnalyser();
-
+    const context = new AudioContext();
+    const source = context.createMediaElementSource(audio);
+    const analyser = context.createAnalyser();
     source.connect(analyser);
     source.connect(context.destination);
 
-    var bufferLength = analyser.frequencyBinCount;
-    var audioData = new Uint8Array(bufferLength);
+    const bufferLength = analyser.frequencyBinCount;
+    const audioData = new Uint8Array(bufferLength);
 
-    var BASS_THRESHOLD = 150;
-    var bassDetected = "";
+    const snowflakes = [NUMBER_OF_SNOWFLAKES];
+    let mag_and_dir = new Array();
+    let currentSnowflake = 0;
 
-    var logo = document.getElementById('logo');
-    var LOGO_DEFAULT_WIDTH = 200;
-    var LOGO_DEFAULT_HEIGHT = 200;
+    let configureCounter = 0;
 
-    var snowflakes = [100];
-    var mag_and_dir = new Array();
-    var currentSnowflake = 0;
+    let magnitudeMultiplier = 5.0
 
-    var configureCounter = 0;
-    var configureSetting = 10;
-
-    var magnitudeMultiplier = 5.0
-
-    for (var i = 0; i < 100; i++) {
-        snowflakes[i] = new Image();
-        snowflakes[i].src = "white_circle.png";
-
-        snowflakes[i].style.position = "absolute";
-        snowflakes[i].style.zIndex = "200";
-        snowflakes[i].style.top = "50%";
-        snowflakes[i].style.left = "50%";
-        snowflakes[i].style.transform = "translate(-50%, -50%)"
-        snowflakes[i].width = 10;
-        snowflakes[i].height = 10;
-
-        mag_and_dir[i] = new Array();
-        mag_and_dir[i].push(0);
-        mag_and_dir[i].push(0);
-
-        document.getElementById('visualizer').appendChild(snowflakes[i]);
+    function createSnowflakes() {
+        for (let i = 0; i < NUMBER_OF_SNOWFLAKES; i++) {
+            snowflakes[i] = new Image();
+            snowflakes[i].src = "white_circle.png";
+    
+            snowflakes[i].style.position = "absolute";
+            snowflakes[i].style.zIndex = "200";
+            snowflakes[i].style.top = "50%";
+            snowflakes[i].style.left = "50%";
+            snowflakes[i].style.transform = "translate(-50%, -50%)"
+            snowflakes[i].width = 10;
+            snowflakes[i].height = 10;
+    
+            mag_and_dir[i] = new Array();
+            mag_and_dir[i].push(0);
+            mag_and_dir[i].push(0);
+    
+            document.getElementById('visualizer').appendChild(snowflakes[i]);
+        }
     }
 
     function determineNumSnowflakes() {
-        var numSnowflakes;
-        var rand = Math.random();
+        const rand = Math.random();
         if (rand <= .7) {
-            numSnowflakes = 1;
+            return 1;
         }
         else if (rand > .7 && rand <= .95) {
-            numSnowflakes = 2;
+            return 2;
         }
-        else {
-            numSnowflakes = 3;
-        }
-        return numSnowflakes;
+        return 3;
     }
 
     function configureSnowflakes(numSnowflakes) {
-        for (var i = 0; i < numSnowflakes; i++) {
+        for (let i = 0; i < numSnowflakes; i++) {
+            snowflakes[i].style.top = "50%";
+            snowflakes[i].style.left = "50%";
+            snowflakes[i].style.transform = "translate(-50%, -50%)"
 
-            snowflakes[currentSnowflake].style.top = "50%";
-            snowflakes[currentSnowflake].style.left = "50%";
-            snowflakes[currentSnowflake].style.transform = "translate(-50%, -50%)"
+            const snowflakeWidth = 5 + 5 * Math.random();
+            snowflakes[i].width = snowflakeWidth;
+            snowflakes[i].height = snowflakeWidth;
 
-            var snowflakeWidth = 5 + 5 * Math.random();
-            snowflakes[currentSnowflake].width = snowflakeWidth;
-            snowflakes[currentSnowflake].height = snowflakeWidth;
+            snowflakes[i].style.opacity = 0.3 + 0.7 * Math.random();
+            snowflakes[i].style.borderRadius = "50%";
+            snowflakes[i].style.boxShadow = "0 0 5px rgba(255,255,255,1)";
 
-            snowflakes[currentSnowflake].style.opacity = 0.3 + 0.7 * Math.random();
-            snowflakes[currentSnowflake].style.borderRadius = "50%";
-            snowflakes[currentSnowflake].style.boxShadow = "0 0 5px rgba(255,255,255,1)";
-
-            var direction = 2 * Math.PI * Math.random();
-            var magnitude = 2 * Math.random();
+            const direction = 2 * Math.PI * Math.random();
+            const magnitude = 2 * Math.random();
 
             mag_and_dir[currentSnowflake][0] = magnitude;
             mag_and_dir[currentSnowflake][1] = direction;
-
-            if (currentSnowflake < 99) {
-                currentSnowflake++;
-            }
-            else {
-                currentSnowflake = 0;
-            }
+            
+            currentSnowflake = currentSnowflake < NUMBER_OF_SNOWFLAKES - 1 ? currentSnowflake + 1 : 0
         }
     }
 
-    function translateSnowflakes() {
-        for (var i = 0; i < 100; i++) {
-            if (snowflakes[i].offsetLeft >= 400 - (800 / 2 + 10)
-                && snowflakes[i].offsetLeft <= 400 + (800 / 2 + 10)
-                && snowflakes[i].offsetTop <= 250 + (500 / 2 + 10)
-                && snowflakes[i].offsetTop >= 250 - (500 / 2 + 10)) {
+    function isWithinFrame(offsetLeft, offsetTop) { 
+        return offsetLeft >= 400 - (800 / 2 + 10)
+        && offsetLeft <= 400 + (800 / 2 + 10)
+        && offsetTop <= 250 + (500 / 2 + 10)
+        && offsetTop >= 250 - (500 / 2 + 10);
+    }
 
-                var velocity_x = magnitudeMultiplier * mag_and_dir[i][0] * Math.cos(mag_and_dir[i][1]) * Math.random();
-                var velocity_y = magnitudeMultiplier * mag_and_dir[i][0] * Math.sin(mag_and_dir[i][1]) * Math.random();
+    function translateSnowflakes() {
+        for (let i = 0; i < NUMBER_OF_SNOWFLAKES; i++) {
+            if (isWithinFrame(snowflakes[i].offsetLeft, snowflakes[i].offsetTop)) {
+                const velocity_x = magnitudeMultiplier * mag_and_dir[i][0] * Math.cos(mag_and_dir[i][1]) * Math.random();
+                const velocity_y = magnitudeMultiplier * mag_and_dir[i][0] * Math.sin(mag_and_dir[i][1]) * Math.random();
 
                 snowflakes[i].style.left = (snowflakes[i].offsetLeft + velocity_x) + "px";
                 snowflakes[i].style.top = (snowflakes[i].offsetTop + velocity_y) + "px";
@@ -115,25 +107,15 @@ window.onload = function () {
     function acquireData() {
         requestAnimationFrame(acquireData);
         analyser.getByteFrequencyData(audioData);
-        //document.getElementById('log').innerHTML = audioData;
-        //console.log(audioData);
-
-        if (audioData[0] > BASS_THRESHOLD) {
-            bassDetected += "|";
-            logo.style.width = (LOGO_DEFAULT_WIDTH + (audioData[0] - BASS_THRESHOLD)) + "px";
-            logo.style.height = (LOGO_DEFAULT_HEIGHT + (audioData[0] - BASS_THRESHOLD)) + "px";
-            magnitudeMultiplier = 1 + 30 * (audioData[0] - BASS_THRESHOLD) / BASS_THRESHOLD;
-        }
-        else {
-            logo.style.width = LOGO_DEFAULT_WIDTH + "px";
-            logo.style.height = LOGO_DEFAULT_HEIGHT + "px";
-            magnitudeMultiplier = 1;
-        }
-        //document.getElementById('bassDetection').innerHTML = bassDetected;
+        
+        const isSignificantBass = audioData[0] > BASS_THRESHOLD;
+        logo.style.width = isSignificantBass ? (LOGO_DEFAULT_WIDTH + (audioData[0] - BASS_THRESHOLD)) + "px" : LOGO_DEFAULT_WIDTH + "px";             
+        logo.style.height = isSignificantBass ? (LOGO_DEFAULT_HEIGHT + (audioData[0] - BASS_THRESHOLD)) + "px" : logo.style.height = LOGO_DEFAULT_HEIGHT + "px";
+        magnitudeMultiplier = isSignificantBass ? 1 + 30 * (audioData[0] - BASS_THRESHOLD) / BASS_THRESHOLD : 1;
     }
 
     function update_snowflakes() {
-        if (configureCounter == configureSetting) {
+        if (configureCounter == CONFIGURE_SETTING) {
             configureSnowflakes(determineNumSnowflakes());
             configureCounter = 0;
         }
@@ -141,6 +123,7 @@ window.onload = function () {
         configureCounter++;
     }
 
+    createSnowflakes();
     acquireData();
     setInterval(update_snowflakes, 1);
 };
